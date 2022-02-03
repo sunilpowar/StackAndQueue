749fdf3aaff50ebf33cdebef623bf1f30efba6b7
sunilpowar <sunilpowarsp8468@gmail.com>
2022-02-04 03:39:06 +0530

    Adding UC2

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -10,6 +10,35 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Stack And Queue");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+            stack.Peek();
+            stack.Pop();
+            stack.Display();
+            stack.Peek();
+            stack.Pop();
+            stack.Display();
+            stack.Peek();
+            stack.Pop();
+            stack.Display();
+            /*stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+           /* LinkedList linkedlist = new LinkedList();
+            linkedlist.Add(56);
+            linkedlist.Add(30);
+            linkedlist.Add(40);
+            linkedlist.Add(70);
+            linkedlist.Display();
+            int position = linkedlist.Search(40);
+            linkedlist.DeleteNodeAtParticularPosition(position);*/
+
+            Console.ReadLine();
         }
     }
 }
